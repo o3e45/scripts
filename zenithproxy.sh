087e880948ccad32c60c29fac762f9afad5cf421
Owen D <owen@owendobson.com>
2025-04-17 18:55:41 -0400

    Update zenithproxy.sh

--- zenithproxy.sh
+++ zenithproxy.sh
@@ -2,16 +2,9 @@
 
 # Update and install required packages
 apt-get update && apt-get upgrade -y
-apt-get install -y tmux zsh unzip zip git python3 python3-pip
+apt-get install -y unzip zip git python3 python3-pip screen
 
 # Change the default shell for the root user to zsh
-chsh -s /usr/bin/zsh root
-
-# Install Oh My Zsh
-cd /root
-wget https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh
-chmod +x install.sh
-/usr/bin/zsh -c "/root/install.sh --unattended"
 
 # Download and unzip ZenithProxy
 cd /root
@@ -22,6 +15,3 @@ unzip ZenithProxy-launcher-linux-amd64.zip
 
 # Clean up the install script
 rm -f /root/install.sh
-
-# Reboot the system
-reboot
